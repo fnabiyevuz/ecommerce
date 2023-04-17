4a586a5fd193b8a1a146a9b23e953c48414d391f
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2023-04-17 16:27:36 +0500

    feat: product models and mixins view_count

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,51 @@
+import admin_thumbnails
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from .models import Category, Company, Product, ProductImage
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+    date_hierarchy = "created_at"
+
+
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+        fields = "__all__"
+        exclude = ("image",)
+
+
+@admin.register(Category)
+class CategoryAdmin(ImportExportModelAdmin):
+    resource_class = CategoryResource
+    list_display = ("name", "slug", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
+    date_hierarchy = "created_at"
+
+
+@admin_thumbnails.thumbnail("image")
+class ProductImageModelAdmin(admin.TabularInline):
+    model = ProductImage
+    extra = 2
+
+
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category", "created_at")
+    list_filter = ("category", "created_at")
+    search_fields = ("name",)
+    date_hierarchy = "created_at"
+    list_editable = ("price",)
+    prepopulated_fields = {"slug": ("name",)}
+    inlines = [ProductImageModelAdmin]
+
+
+admin.site.register(Product, ProductModelAdmin)
