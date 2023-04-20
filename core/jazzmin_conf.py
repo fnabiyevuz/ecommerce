52f8518d5b61f9cd357e05df88ec231d828e0963
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2023-04-20 13:14:19 +0500

    feat: admin captcha

--- core/jazzmin_conf.py
+++ core/jazzmin_conf.py
@@ -2,11 +2,11 @@ from typing import Any, Dict  # noqa: F401
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Default Title",
+    "site_title": "E-Commerce",
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Default Header",
+    "site_header": "E-Commerce",
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Default Brand",
+    "site_brand": "E-Commerce",
     # Logo to use for your site, must be present in static files, used for brand on top left
     "site_logo": "logos/admin_logo_big.png",
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
